be7db141982c580f7d3b89c3d57a1889dc7ec05e
Rendall Schijven <rendall.schijven@hotmail.com>
2017-03-21 11:20:04 +0100

    Insert werkt

---
+++ App_Code/Data/TableMethods.cs
@@ -0,0 +1,38 @@
+﻿using Modulewijzer.Models;
+using System.Data;
+
+
+namespace Modulewijzer.Data
+{
+    public sealed class TableMethods
+    {
+
+
+        /// <summary>
+        /// Inserts the given method into the database.
+        /// </summary>
+        /// <param name="method">The method to insert.</param>
+        public void Create(Method method)
+        {
+
+        }
+
+        /// <summary>
+        /// Updates the given method within the database.
+        /// </summary>
+        /// <param name="method">The method to update.</param>
+        public void Update(Method method)
+        {
+
+        }
+
+        /// <summary>
+        /// Deletes the given method from the database.
+        /// </summary>
+        /// <param name="method">The method to delete.</param>
+        public void Delete(Method method)
+        {
+
+        }
+    }
+}
